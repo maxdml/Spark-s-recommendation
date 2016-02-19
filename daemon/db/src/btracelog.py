0419fe62b94b1844eba21a53767d47c79b718fce
maxdml@cs.duke.edu <maxdml@cs.duke.edu>
2016-02-18 19:33:25 -0500

    Add details of heap/non heap and system/process cpu measurements from btracelog

--- daemon/db/src/btracelog.py
+++ daemon/db/src/btracelog.py
@@ -4,7 +4,10 @@ class BtraceLog:
     def __init__(self, btracelog_fname):
         self.btracelog_fname = btracelog_fname
         self.max_memory = None
-        self.avg_cpu_load = None
+        self.max_heap = None
+        self.max_non_heap = None
+        self.avg_process_cpu_load = None
+        self.avg_system_cpu_load = None
 
         self.parse()
 
@@ -12,6 +15,9 @@ class BtraceLog:
         return "(" + str(self.max_memory) + "," + str(self.avg_cpu_load) + ")"
 
     def parse(self):
-        time, memory, cpu = np.loadtxt(self.btracelog_fname, unpack=True, delimiter=",", usecols=(0,3,4))
+        time, max_heap, max_non_heap, memory, process_cpu, system_cpu = np.loadtxt(self.btracelog_fname, unpack=True, delimiter=",", usecols=(0,1,2,3,4,5))
         self.max_memory = max(memory)
-        self.avg_cpu_load = sum(cpu) / len(cpu)
+        self.max_heap = max(max_heap)
+        self.max_non_heap = max(max_non_heap)
+        self.avg_process_cpu_load = sum(process_cpu) / len(process_cpu)
+        self.avg_system_cpu_load = sum(system_cpu) / len(system_cpu)
