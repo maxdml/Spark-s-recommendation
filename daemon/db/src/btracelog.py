b60fd74386b169435e81e063f2ea785168bc7be4
maxdml@cs.duke.edu <maxdml@cs.duke.edu>
2016-02-21 09:51:53 -0500

    add average metrics to btracelog object

--- daemon/db/src/btracelog.py
+++ daemon/db/src/btracelog.py
@@ -4,17 +4,17 @@ class BtraceLog:
   def __init__(self, btracelog_fname):
     self.btracelog_fname = btracelog_fname
     self.max_memory = None
+    self.avg_memory_usage = None
     self.max_heap = None
     self.avg_heap_usage = None
     self.max_non_heap = None
+    self.avg_non_heap_usage = None
     self.avg_process_cpu_load = None
     self.avg_system_cpu_load = None
     self.heap = None
     self.non_heap = None
     self.memory = None
     self.process_cpu = None
-    self.system_cpu = None
-    self.time = None
 
     self.executor_id = self.btracelog_fname.split('@')[1].split('.')[0]
     self.executor_id += "-"
@@ -31,5 +31,7 @@ class BtraceLog:
     self.max_heap = max(self.heap)
     self.max_non_heap = max(self.non_heap)
     self.avg_heap_usage = sum(self.heap) / len(self.heap)
+    self.avg_non_heap_usage = sum(self.non_heap) / len(self.non_heap)
+    self.avg_memory_usage = sum(self.memory) / len(self.memory)
     self.avg_process_cpu_load = sum(self.process_cpu) / len(self.process_cpu)
     self.avg_system_cpu_load = sum(self.system_cpu) / len(self.system_cpu)
